3b7ef85b76e28f6090598ac88237f3e62f724a2c
João Pedro <joao21dev@gmail.com>
2022-08-09 17:54:04 -0300

    sidebar logo added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,15 +3,13 @@ import PropTypes from 'prop-types'
 import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
 import Drawer from '@mui/material/Drawer'
-import InboxIcon from '@mui/icons-material/MoveToInbox'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
-import MailIcon from '@mui/icons-material/Mail'
 import Toolbar from '@mui/material/Toolbar'
-import logo from '../assets/logo-1.png'
+import logo from '../assets/logo-2.png'
 import {
   CheckBoxOutlineBlankOutlined,
   DraftsOutlined,
@@ -57,7 +55,7 @@ export default function ResponsiveDrawer(props: any) {
     <div>
       <Divider />
       <Toolbar>
-        <img src={logo} alt='logo' />
+        <img style={{ height: '75px' }} src={logo} alt='logo' />
       </Toolbar>
       <Divider />
 
@@ -97,6 +95,12 @@ export default function ResponsiveDrawer(props: any) {
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
 
         <Drawer
+          PaperProps={{
+            sx: {
+              backgroundColor: 'pink',
+              color: 'red',
+            },
+          }}
           container={container}
           variant='temporary'
           open={mobileOpen}
